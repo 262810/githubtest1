575c093349dcde2c62b7e3a5f5c3cb4700eebee3
262810 <261248@edu.p.lodz.pl>
2025-10-06 09:30:19 +0200

    /d/11

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int Policz(int a, int b) {
 int main() {
     std::cout << "Hello World!\n";
     std::cout << "W pierwszym branchu, 2 * 3 = " <<Policz(2,3)<<std::endl;
-    //dt
+    //dt1
     return 0;
     }
 
